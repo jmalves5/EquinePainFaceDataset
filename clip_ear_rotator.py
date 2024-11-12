b906042ba3ea336c98164043bb405e62d04e53f0
Joao Alves <jmal@create.aau.dk>
2024-11-12 14:45:41 +0100

    update clips path

--- clip_ear_rotator.py
+++ clip_ear_rotator.py
@@ -7,12 +7,12 @@ import subprocess
 
 # Define base folders
 base_path = "/home/joao/workspace/EquinePainFaceDataset/"
-videos_path = base_path + "CleanAnEquinePainFaceDataset/videos/"
+videos_path = base_path + "CleanAnEquinePainFaceDataset/videos/cropped_videos/"
 annotations_file_path = base_path + "CleanAnEquinePainFaceDataset/JSONAnnotations/annotations.json" 
-FPS=46.875
+FPS=25
 
 # Create output folder
-output_dir = base_path + "CleanAnEquinePainFaceDataset/EAD_104_Clips"
+output_dir = base_path + "CleanAnEquinePainFaceDataset/EAD_104_Clips/cropped_EAD_104_Clips"
 
 try:
     os.makedirs(output_dir, exist_ok=True)
@@ -47,13 +47,13 @@ with open(annotations_file_path) as json_file:
                 # print(action_unit['End time'])
 
                 # Convert start time to frames
-                action_start = str(get_sec(action_unit['Start time'])-1)
-                action_end = str(get_sec(action_unit['End time']))
+                action_start = str(get_sec(action_unit['Start time']))
+                #action_end = str(get_sec(action_unit['End time']))
                 duration = str(int(math.ceil(action_unit['Duration (s)']))+1)
                 # Clip the file
                 input_file_path = videos_path + video
 
-                subprocess.run(["ffmpeg", "-ss", action_start, "-i", input_file_path, "-c", "copy", "-t", duration, f"{output_dir}/clip_{i}_{action_unit['Code']}_{video}"])
+                subprocess.run(["ffmpeg", "-y", "-ss", action_start, "-i", input_file_path, "-c", "copy", "-t", duration, f"{output_dir}/clip_{i}_{action_unit['Code']}_{video}"])
                 # create gifs: ffmpeg -i CleanAnEquinePainFaceDataset/EAD_104_Clips/clip_2_EAD104R_S5.mp4  -r 30   -vf scale=1080:-1   -ss 00:00:00 -to 00:00:02   blink.gif
 
                 i = i + 1
